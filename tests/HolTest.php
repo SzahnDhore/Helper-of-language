939f538165d3ecbd30b0e2e18dfa1aa7f48858f0
Staffan Lindsgård <szandor@szandor.com>
2013-10-29 23:55:01 +0100

    Setting up tests.

--- tests/HolTest.php
+++ tests/HolTest.php
@@ -3,16 +3,23 @@
 // --- Includes the main language file.
 require_once dirname(__FILE__).'/../src/HolQaH/lang/class.language.php';
 
+// --- Mocks a file system.
+require_once 'vfsStream/vfsStream.php';
+
 // --- Test class.
 class languageTest extends PHPUnit_Framework_TestCase {
 
 	private $lang = null;
 
 	public function setUp() {
+		vfsStream::setup('someDir');
+		$this->driver = new VGF_Storage_Driver_Filesystem();
+		$this->driver->rootDir = vfsStream::url('someDir');
 		$this->lang = new HolQaH\language;
 	}
 
 	public function tearDown() {
+		$this->driver = null;
 		$this->lang = null;
 	}
 
@@ -24,9 +31,9 @@ class languageTest extends PHPUnit_Framework_TestCase {
 		$this->assertInstanceOf('HolQaH\language', $this->lang);
 	}
 
-	// public function testSettings() {
-		// $this->assertObjectHasAttribute('phrase', $this->lang);
-	// }
+	public function testSettings() {
+		$this->assertObjectHasAttribute('phrase', $this->lang);
+	}
 
 }
 
