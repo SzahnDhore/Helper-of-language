12cbf18a5a4a27a8cd5b5615636551f216b1fe90
Staffan Lindsgård <szandor@szandor.com>
2013-10-29 21:23:00 +0100

    Main script should now be properly included in the test file.

--- tests/HolTest.php
+++ tests/HolTest.php
@@ -1,34 +1,28 @@
 <?php
 
+// --- Includes the main language file.
+require_once dirname(__FILE__).'/../src/HolQaH/lang/class.language.php';
+
+// --- Test class.
 class HolTest extends PHPUnit_Framework_TestCase {
 
-
 	private $lang = null;
-
-
 
 	public function setUp() {
 		$this->lang = new HolQaH\language;
 	}
 
-
-
 	public function tearDown() {
 		$this->lang = null;
 	}
-
-
 
 	public function testInstanceOf() {
 		$this->assertInstanceOf('HolQaH\language',$this->lang);
 	}
 
-
-
 	public function testInvalidArgumentIsProperlyThrown() {
 		$this->lang->phrase('nonExistentPhrase');
 	}
-
 
 }
 
